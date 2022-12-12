9d3ef92b6b3f2626977666921a818a4a92982308
Arjun <dotadexarjun2002@gmail.com>
2022-12-12 18:26:03 +0530

    changed

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,16 @@
 <h1>{{$heading}}</h1>
+
+@unless(count($listings)==0)
+
+
 @foreach ($listings as $listing)
-    <h2><?php echo $listing['title']; ?></h2>
-    <p><?php echo $listing['description']; ?></p>
-    @endforeach
+    <h2>{{$listing['title']}}</h2>
+    <p>{{$listing['description']}}</p>
+
+@endforeach
+
+
+    
+@else
+    <p>No listings Found</p>
+    @endunless
