7d1fd7b3f854587e53a8b45910b3675084d52402
Arjun <dotadexarjun2002@gmail.com>
2022-12-14 14:04:24 +0530

    removed 2 api's

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dummyAPI;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +20,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("data",[dummyAPI::class,'getdata']);
